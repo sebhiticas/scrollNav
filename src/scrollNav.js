49a4f18e63b23c780b430661bc05766ff7090426
Sebastien Hiticas <sebastien.hiticas@gmail.com>
2018-02-07 09:26:42 -0500

    - Reformat - Add events: onFixed, onUnfixed - Add stickInParent options

--- src/scrollNav.js
+++ src/scrollNav.js
@@ -6,25 +6,25 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+(function ($) {
 
   // Animate scrolling to section location
-  var scroll_to = function(value, speed, offset, animated) {
-    if ( $(value).length > 0 ) {
+  var scroll_to = function (value, speed, offset, animated) {
+    if ($(value).length > 0) {
       var destination = $(value).offset().top;
       speed = animated ? speed : 0;
 
-	    // Add a class to the scrolled-to section
-	    $('.' + S.settings.className + '__focused-section').removeClass(S.settings.className + '__focused-section');
-	    $(value).addClass(S.settings.className + '__focused-section');
+      // Add a class to the scrolled-to section
+      $('.' + S.settings.className + '__focused-section').removeClass(S.settings.className + '__focused-section');
+      $(value).addClass(S.settings.className + '__focused-section');
 
       $('html:not(:animated),body:not(:animated)')
-        .animate({scrollTop: destination - offset }, speed );
+        .animate({ scrollTop: destination - offset }, speed);
     }
   };
 
   // Get url hash if one exists
-  var get_hash = function() {
+  var get_hash = function () {
     return window.location.hash;
   };
 
@@ -53,9 +53,12 @@
       onInit: null,
       onRender: null,
       onDestroy: null,
-      onResetPos: null
+      onResetPos: null,
+      onFixed: null,
+      onUnfixed: null,
+      stickInParent: false,
     },
-    _set_body_class: function(state) {
+    _set_body_class: function (state) {
       // Set and swap our loading hooks to the body
 
       var $body = $('body');
@@ -68,7 +71,7 @@
         $body.removeClass(S.classes.loading).addClass(S.classes.failed);
       }
     },
-    _find_sections: function($el) {
+    _find_sections: function ($el) {
       // Find the html for each section
 
       var target_elems = S.settings.sections;
@@ -77,104 +80,104 @@
       if (S.settings.showTopLink) {
         var $firstElem = $el.children().first();
 
-        if ( !$firstElem.is(target_elems) ) {
-          raw_html.push( $firstElem.nextUntil(target_elems).addBack() );
+        if (!$firstElem.is(target_elems)) {
+          raw_html.push($firstElem.nextUntil(target_elems).addBack());
         }
       }
 
-      $el.find(target_elems).each(function() {
-        raw_html.push( $(this).nextUntil(target_elems).addBack() );
+      $el.find(target_elems).each(function () {
+        raw_html.push($(this).nextUntil(target_elems).addBack());
       });
 
       S.sections = {
         raw: raw_html
       };
     },
-    _setup_sections: function(sections) {
+    _setup_sections: function (sections) {
       // Wrap each section and add it's details to the section array
 
       var section_data = [];
 
-      $(sections).each(function(i) {
-        var sub_data      = [];
+      $(sections).each(function (i) {
+        var sub_data = [];
         var $this_section = $(this);
-        var section_id    = 'scrollNav-' + (i + 1);
-        var isFirst       = function() { return i === 0; };
-        var hasHeading    = function() { return !$this_section.eq(0).is(S.settings.sections); };
-        var text          = ( S.settings.showTopLink && isFirst() && hasHeading() ) ? S.settings.topLinkText : $this_section.filter(S.settings.sections).text();
+        var section_id = 'scrollNav-' + (i + 1);
+        var isFirst = function () { return i === 0; };
+        var hasHeading = function () { return !$this_section.eq(0).is(S.settings.sections); };
+        var text = (S.settings.showTopLink && isFirst() && hasHeading()) ? S.settings.topLinkText : $this_section.filter(S.settings.sections).text();
 
         $this_section.wrapAll('<' + S.settings.sectionElem + ' id="' + section_id + '" class="' + S.settings.className + '__section" />');
 
         if (S.settings.subSections) {
-          var $sub_sections  = $this_section.filter(S.settings.subSections);
+          var $sub_sections = $this_section.filter(S.settings.subSections);
 
           if ($sub_sections.length > 0) {
-            $sub_sections.each(function(i) {
-              var sub_id      = section_id + '-' + (i + 1);
-              var sub_text    = $(this).text();
-              var $this_sub   = $this_section.filter($(this).nextUntil($sub_sections).addBack());
+            $sub_sections.each(function (i) {
+              var sub_id = section_id + '-' + (i + 1);
+              var sub_text = $(this).text();
+              var $this_sub = $this_section.filter($(this).nextUntil($sub_sections).addBack());
 
               $this_sub.wrapAll('<div id="' + sub_id + '" class="' + S.settings.className + '__sub-section" />');
-              sub_data.push( {id: sub_id, text: sub_text} );
+              sub_data.push({ id: sub_id, text: sub_text });
             });
           }
         }
 
-        section_data.push( {id: section_id, text: text, sub_sections: sub_data} );
+        section_data.push({ id: section_id, text: text, sub_sections: sub_data });
       });
 
       S.sections.data = section_data;
     },
-    _tear_down_sections: function(sections) {
-      $(sections).each(function() {
+    _tear_down_sections: function (sections) {
+      $(sections).each(function () {
         var sub_sections = this.sub_sections;
 
         $('#' + this.id).children().unwrap();
 
         if (sub_sections.length > 0) {
-          $(sub_sections).each(function() {
+          $(sub_sections).each(function () {
             $('#' + this.id).children().unwrap();
           });
         }
       });
     },
-    _setup_nav: function(sections) {
-    // Populate an ordered list from the section array we built
+    _setup_nav: function (sections) {
+      // Populate an ordered list from the section array we built
 
-      var $headline = $('<span />', {'class': S.settings.className + '__heading', text: S.settings.headlineText});
-      var $wrapper  = $('<div />', {'class': S.settings.className + '__wrapper'});
-      var $nav      = $('<nav />', {'class': S.settings.className, 'role': 'navigation'});
-      var $nav_list = $('<ol />', {'class': S.settings.className + '__list'});
+      var $headline = $('<span />', { 'class': S.settings.className + '__heading', text: S.settings.headlineText });
+      var $wrapper = $('<div />', { 'class': S.settings.className + '__wrapper' });
+      var $nav = $('<nav />', { 'class': S.settings.className, 'role': 'navigation' });
+      var $nav_list = $('<ol />', { 'class': S.settings.className + '__list' });
 
-      $.each(sections, function(i) {
-        var $item     = (i === 0) ? $('<li />', {'class': S.settings.className + '__item active'}) : $('<li />', {'class': S.settings.className + '__item'});
-        var $link     = $('<a />', {'href': '#' + this.id, 'class': S.settings.className + '__link', text: this.text});
+      $.each(sections, function (i) {
+        var $item = (i === 0) ? $('<li />', { 'class': S.settings.className + '__item active' }) : $('<li />', { 'class': S.settings.className + '__item' });
+        var $link = $('<a />', { 'href': '#' + this.id, 'class': S.settings.className + '__link', text: this.text });
         var $sub_nav_list;
 
         if (this.sub_sections.length > 0) {
           $item.addClass('is-parent-item');
-          $sub_nav_list = $('<ol />', {'class': S.settings.className + '__sub-list'});
+          $sub_nav_list = $('<ol />', { 'class': S.settings.className + '__sub-list' });
 
-          $.each(this.sub_sections, function() {
-            var $sub_item = $('<li />', {'class': S.settings.className + '__sub-item'});
-            var $sub_link = $('<a />', {'href': '#' + this.id, 'class': S.settings.className + '__sub-link', text: this.text});
+          $.each(this.sub_sections, function () {
+            var $sub_item = $('<li />', { 'class': S.settings.className + '__sub-item' });
+            var $sub_link = $('<a />', { 'href': '#' + this.id, 'class': S.settings.className + '__sub-link', text: this.text });
 
-            $sub_nav_list.append( $sub_item.append($sub_link) );
+            $sub_nav_list.append($sub_item.append($sub_link));
           });
         }
 
-        $nav_list.append( $item.append($link).append($sub_nav_list) );
+        $nav_list.append($item.append($link).append($sub_nav_list));
       });
 
       if (S.settings.showHeadline) {
-        $nav.append( $wrapper.append($headline).append($nav_list) );
+        $nav.append($wrapper.append($headline).append($nav_list));
       } else {
-        $nav.append( $wrapper.append($nav_list) );
+        $nav.append($wrapper.append($nav_list));
       }
 
       S.nav = $nav;
     },
-    _insert_nav: function() {
+    _insert_nav: function () {
       // Add the nav to our page
 
       var insert_location = S.settings.insertLocation;
@@ -182,59 +185,65 @@
 
       S.nav[insert_location]($insert_target);
     },
-    _setup_pos: function() {
+    _setup_pos: function () {
       // Find the offset positions of each section
 
-      var $nav        = S.nav;
-      var vp_height   = $(window).height();
-      var nav_offset  = $nav.offset().top;
+      var $nav = S.nav;
+      var vp_height = $(window).height();
+      var nav_offset = $nav.offset().top;
 
-      var set_offset = function(section) {
-        var $this_section  = $('#' + section.id);
-        var this_height    = $this_section.height();
+      var set_offset = function (section) {
+        var $this_section = $('#' + section.id);
+        var this_height = $this_section.height();
 
-        section.top_offset    = $this_section.offset().top;
+        section.top_offset = $this_section.offset().top;
         section.bottom_offset = section.top_offset + this_height;
       };
 
-      $.each(S.sections.data, function() {
+      $.each(S.sections.data, function () {
         set_offset(this);
 
-        $.each(this.sub_sections, function() {
+        $.each(this.sub_sections, function () {
           set_offset(this);
         });
       });
 
       S.dims = {
-        vp_height:  vp_height,
+        vp_height: vp_height,
         nav_offset: nav_offset
       };
     },
-    _check_pos: function() {
+    _check_pos: function () {
       // Set nav to fixed after scrolling past the header and add an in-view class to any
       // sections currently within the bounds of our view and active class to the first
       // in-view section
 
-      var $nav                = S.nav;
-      var win_top             = $(window).scrollTop();
-      var boundry_top         = win_top + S.settings.scrollOffset;
-      var boundry_bottom      = win_top + S.dims.vp_height - S.settings.scrollOffset;
-      var sections_active     = [];
+      var $nav = S.nav;
+      var win_top = $(window).scrollTop();
+      var boundry_top = win_top + S.settings.scrollOffset;
+      var boundry_bottom = win_top + S.dims.vp_height - S.settings.scrollOffset;
+      var sections_active = [];
       var sub_sections_active = [];
 
-      if ( win_top > (S.dims.nav_offset - S.settings.fixedMargin) ) { $nav.addClass('fixed'); }
-      else { $nav.removeClass('fixed'); }
+      if (win_top > (S.dims.nav_offset - S.settings.fixedMargin)) {
+        $nav.addClass('fixed');
+        if (S.settings.onFixed) { S.settings.onFixed.call(this); }
+      }
+      else {
+        $nav.removeClass('fixed');
+        if (S.settings.onUnfixed) { S.settings.onUnfixed.call(this); }
+      }
 
-      var in_view = function(section) {
+      var in_view = function (section) {
         return (section.top_offset >= boundry_top && section.top_offset <= boundry_bottom) || (section.bottom_offset > boundry_top && section.bottom_offset < boundry_bottom) || (section.top_offset < boundry_top && section.bottom_offset > boundry_bottom);
       };
 
-      $.each(S.sections.data, function() {
-        if ( in_view(this) ) {
+      $.each(S.sections.data, function () {
+        if (in_view(this)) {
           sections_active.push(this);
         }
-        $.each(this.sub_sections, function() {
-          if ( in_view(this) ) {
+        $.each(this.sub_sections, function () {
+          if (in_view(this)) {
             sub_sections_active.push(this);
           }
         });
@@ -243,7 +252,7 @@
       $nav.find('.' + S.settings.className + '__item').removeClass('active').removeClass('in-view');
       $nav.find('.' + S.settings.className + '__sub-item').removeClass('active').removeClass('in-view');
 
-      $.each(sections_active, function(i) {
+      $.each(sections_active, function (i) {
         if (i === 0) {
           $nav.find('a[href="#' + this.id + '"]').parents('.' + S.settings.className + '__item').addClass('active').addClass('in-view');
         } else {
@@ -252,7 +261,24 @@
       });
       S.sections.active = sections_active;
 
-      $.each(sub_sections_active, function(i) {
+      if (S.settings.stickInParent) {
+        var $wrapper = $nav.find('.' + S.settings.className + '__wrapper');
+        if (!sections_active.length) {
+          $wrapper.css({
+            position: 'absolute',
+            bottom: 0,
+            top: 'auto'
+          });
+        } else {
+          $wrapper.css({
+            position: '',
+            bottom: '',
+            top: ''
+          });
+        }
+      }
+
+      $.each(sub_sections_active, function (i) {
         if (i === 0) {
           $nav.find('a[href="#' + this.id + '"]').parents('.' + S.settings.className + '__sub-item').addClass('active').addClass('in-view');
         } else {
@@ -260,58 +286,58 @@
         }
       });
     },
-    _init_scroll_listener: function() {
+    _init_scroll_listener: function () {
       // Set a scroll listener to update the fixed and active classes
 
-      $(window).on('scroll.scrollNav', function() {
+      $(window).on('scroll.scrollNav', function () {
         S._check_pos();
       });
     },
-    _rm_scroll_listeners: function() {
+    _rm_scroll_listeners: function () {
       $(window).off('scroll.scrollNav');
     },
-    _init_resize_listener: function() {
+    _init_resize_listener: function () {
       // Set a resize listener to update position values and the fixed and active classes
 
-      $(window).on('resize.scrollNav', function() {
+      $(window).on('resize.scrollNav', function () {
         S._setup_pos();
         S._check_pos();
       });
     },
-    _rm_resize_listener: function() {
+    _rm_resize_listener: function () {
       $(window).off('resize.scrollNav');
     },
-    _init_click_listener: function() {
+    _init_click_listener: function () {
       // Scroll to section on click
 
-      $('.' + S.settings.className).find('a').on('click.scrollNav', function(e) {
+      $('.' + S.settings.className).find('a').on('click.scrollNav', function (e) {
         e.preventDefault();
 
-        var value     = $(this).attr('href');
-        var speed     = S.settings.speed;
-        var offset    = S.settings.scrollOffset;
-        var animated  = S.settings.animated;
+        var value = $(this).attr('href');
+        var speed = S.settings.speed;
+        var offset = S.settings.scrollOffset;
+        var animated = S.settings.animated;
 
         scroll_to(value, speed, offset, animated);
       });
     },
-    _rm_click_listener: function() {
+    _rm_click_listener: function () {
       $('.' + S.settings.className).find('a').off('click.scrollNav');
     },
-    _init_keyboard_listener: function(sections) {
+    _init_keyboard_listener: function (sections) {
       // Scroll to section on arrow key press
 
       if (S.settings.arrowKeys) {
-        $(document).on('keydown.scrollNav', function(e) {
+        $(document).on('keydown.scrollNav', function (e) {
           if (e.keyCode === 40 || e.keyCode === 38) {
-            var findSection = function(key) {
+            var findSection = function (key) {
               var i = 0;
               var l = sections.length;
 
               for (i; i < l; i++) {
                 if (sections[i].id === S.sections.active[0].id) {
-                  var array_offset  = (key === 40) ? i + 1 : i -1;
-                  var id            = (sections[array_offset] === undefined) ? undefined : sections[array_offset].id;
+                  var array_offset = (key === 40) ? i + 1 : i - 1;
+                  var id = (sections[array_offset] === undefined) ? undefined : sections[array_offset].id;
 
                   return id;
                 }
@@ -323,10 +349,10 @@
             if (target_section !== undefined) {
               e.preventDefault();
 
-              var value     = '#' + target_section;
-              var speed     = S.settings.speed;
-              var offset    = S.settings.scrollOffset;
-              var animated  = S.settings.animated;
+              var value = '#' + target_section;
+              var speed = S.settings.speed;
+              var offset = S.settings.scrollOffset;
+              var animated = S.settings.animated;
 
               scroll_to(value, speed, offset, animated);
             }
@@ -334,11 +360,11 @@
         });
       }
     },
-    _rm_keyboard_listener: function() {
+    _rm_keyboard_listener: function () {
       $(document).off('keydown.scrollNav');
     },
-    init: function(options) {
-      return this.each(function() {
+    init: function (options) {
+      return this.each(function () {
         var $el = $(this);
 
         // Merge default settings with user defined options
@@ -356,13 +382,13 @@
           S._set_body_class('loading');
           S._find_sections($el);
 
-          if ( $el.find(S.settings.sections).length > 0 ) {
+          if ($el.find(S.settings.sections).length > 0) {
             // BUILD!!!!
 
             S._setup_sections(S.sections.raw);
             S._setup_nav(S.sections.data);
 
-            if ( S.settings.insertTarget.length > 0 ) {
+            if (S.settings.insertTarget.length > 0) {
               //Add to page
 
               S._insert_nav();
@@ -373,8 +399,19 @@
               S._init_click_listener();
               S._init_keyboard_listener(S.sections.data);
               S._set_body_class('success');
-              if (S.settings.scrollToHash){
-                scroll_to( get_hash() );
+              if (S.settings.scrollToHash) {
+                scroll_to(get_hash());
+              }
+
+              if(S.settings.stickInParent) {
+                var $nav = S.nav;
+                var $wrapper = $nav.find('.' + S.settings.className + '__wrapper');
+                $nav.css({
+                  height: $wrapper.outerHeight()
+                });
+                $wrapper.css({
+                  width: $nav.width()
+                })
               }
 
               // Fire custom render callback
@@ -396,8 +433,8 @@
         }
       });
     },
-    destroy: function() {
-      return this.each(function() {
+    destroy: function () {
+      return this.each(function () {
 
         // Unbind event listeners
         S._rm_scroll_listeners();
@@ -422,7 +459,7 @@
         S.sections = undefined;
       });
     },
-    resetPos: function() {
+    resetPos: function () {
       S._setup_pos();
       S._check_pos();
 
@@ -431,24 +468,24 @@
     }
   };
 
-  $.fn.scrollNav = function() {
+  $.fn.scrollNav = function () {
     var options;
-    var method  = arguments[0];
+    var method = arguments[0];
 
     if (S[method]) {
       // Method exists, so use it
 
-      method  = S[method];
+      method = S[method];
       options = Array.prototype.slice.call(arguments, 1);
-    } else if (typeof(method) === 'object' || !method) {
+    } else if (typeof (method) === 'object' || !method) {
       // No method passed, default to init
 
-      method  = S.init;
+      method = S.init;
       options = arguments;
     } else {
       // Method doesn't exist
 
-      $.error( 'Method ' +  method + ' does not exist in the scrollNav plugin' );
+      $.error('Method ' + method + ' does not exist in the scrollNav plugin');
       return this;
     }
 
